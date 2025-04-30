6865b2366457061585648c6ab0a1b02ebf2d7c20
Shue84 <suyanti.witono@bpkpenaburjakarta.or.id>
2025-04-30 15:38:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from prediction import prediction
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("https://github.com/Shue84/Jaya-Jaya/blob/main/JAYA%20JAYA.png", width=130)
+    st.image("main/JAYA%20JAYA.png", width=130)
 with col2:
     st.header('Students Dropout Prediction (Prototype)')
 
